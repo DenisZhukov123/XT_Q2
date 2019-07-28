103f06ab28fc3d8fec0bfc960f39248c2ab84d52
ZhukovDO <deniska000@mail.ru>
2019-07-28 21:15:52 +0400

    Task04_Delagates_and_Extensions

---
+++ Task04_Delegates_and_Extensions/4.4_Number_array_sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _4._4_Number_array_sum
+{
+    public static class NumberArraySum
+    {
+        public static T SumMass<T>(this T[] param)
+        {
+            if (param == null)
+                throw new ArgumentNullException();
+            double sum = 0;
+            for (int i = 0; i < param.Length; i++)
+                sum += Convert.ToDouble(param[i]);
+            return (T)Convert.ChangeType(sum, typeof(T));
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] testmass1 = new int[] { 1, 4, 5, 3, 5, 3, 7 };
+            Console.WriteLine("Summ = {0}", testmass1.SumMass());
+
+            float[] testmass2 = new float[] { 1.2f, 4f, 5.1f, 3.7f, 5f, 3f, 7f };
+            Console.WriteLine("Summ = {0}", testmass2.SumMass());
+
+            double[] testmass3 = new double[] { 1.2, 4, 3.5, 8.5, 5.4, 3, 7 };
+            Console.WriteLine("Summ = {0}", testmass3.SumMass());
+
+            decimal[] testmass4 = new decimal[] { 1.2m, 2.1m, 3.3m, 4m, 5m, 6m, 7m, 8m, 9m, 10m};
+            Console.WriteLine("Summ = {0}", testmass4.SumMass());
+        }
+    }
+}
---
+++ Task04_Delegates_and_Extensions/4.6_I_seek_you/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._6_I_seek_you
+{
+    class Program
+    {
+        public delegate bool Positive(int a);
+        public static List<int> FindAllPositive(int[] mass)
+        {
+            if (mass == null)
+                throw new ArgumentNullException();
+            List<int> result = new List<int>();
+            for (int i = 0; i < mass.Length; i++)
+                if (mass[i] > 0)
+                    result.Add(mass[i]);
+            return result;
+        }
+
+        public static List<int> FindAllPositiveDelegate(int[] mass, Positive pos)
+        {
+            if (mass == null||pos==null)
+                throw new ArgumentNullException();
+            List<int> result = new List<int>();
+            for (int i = 0; i < mass.Length; i++)
+                if (pos(mass[i]))
+                    result.Add(mass[i]);
+            return result;
+        }
+
+        public static int[] FindAllPositiveLinq(int[] mass)
+        {
+            if (mass == null)
+                throw new ArgumentNullException();
+            var query = from item in mass where item > 0 select item;
+            return query.ToArray();
+        }
+
+
+        public static List<int> FindAllPositiveAnon(int[] mass, Positive pos)
+        {
+            if (mass == null||pos==null)
+                throw new ArgumentNullException();
+            List<int> result = new List<int>();
+            for (int i = 0; i < mass.Length; i++)
+                if (pos(mass[i]))
+                    result.Add(mass[i]);
+            return result;
+        }
+
+        public static bool IsPositive(int a)
+        {
+            if (a > 0)
+                return true;
+            return false;
+        }
+
+        static void Main()
+        {
+            int[] massive =new int[]{-1,2,5,-3,5,-6,-2,7,8};
+            List<int> result1 = new List<int>();
+            List<int> result2 = new List<int>();
+            List<int> result3 = new List<int>();
+            List<int> result4 = new List<int>();
+            result1 =FindAllPositive(massive);
+            result2=FindAllPositiveDelegate(massive,IsPositive);
+            Console.WriteLine("Result for simple void:");
+            foreach (var item in result1)
+                Console.Write("{0} ", item);
+            Console.WriteLine("");
+            Console.WriteLine("Result for void delegate:");
+            foreach (var item in result2)
+                Console.Write("{0} ", item);
+            Console.WriteLine("");
+            Positive pos_anon = delegate (int a)
+            {
+                if (a > 0)
+                    return true;
+                return false;
+            };
+            result3 = FindAllPositiveAnon(massive, pos_anon);
+            Console.WriteLine("Result for anon void:");
+            foreach (var item in result3)
+                Console.Write("{0} ", item);
+            Console.WriteLine("");
+            Positive pos_lambda =  (a) =>
+            {
+                if (a > 0)
+                    return true;
+                return false;
+            };
+            result4 = FindAllPositiveAnon(massive, pos_lambda);
+            Console.WriteLine("Result for lambda void:");
+            foreach (var item in result4)
+                Console.Write("{0} ", item);
+            Console.WriteLine("");
+            Console.WriteLine("Result for LINQ void:");
+            foreach (var item in FindAllPositiveLinq(massive))
+                Console.Write("{0} ", item);
+            Console.WriteLine("");
+        }
+    }
+}
